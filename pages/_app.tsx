39e234d44d07379187da3d653f5ea7eed6590be6
Grady Arnold <gradyarnold11@gmail.com>
2023-02-06 00:46:07 +0800

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 const theme = extendTheme({
   backgroundColor: "#000",
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
   <ChakraProvider theme={theme}>
     <Component {...pageProps} />
+    <Analytics />
   </ChakraProvider>
   );
 }
